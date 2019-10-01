2fb1c5d2a4c40486b369fb22748549d5fa417719
derthek <shape07_mrm@hotmail.com>
2019-10-01 15:09:08 +0200

    Updates

---
+++ precache-manifest.cb0d0165fc374ae76558db189187abad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156fae1eda10b0d5a5ce1acf535fa3c4",
+    "url": "/DFDS/index.html"
+  },
+  {
+    "revision": "5c755d10078e71356297",
+    "url": "/DFDS/static/css/main.6210ad8c.chunk.css"
+  },
+  {
+    "revision": "b63150b327024afccf22",
+    "url": "/DFDS/static/js/2.9bdd2024.chunk.js"
+  },
+  {
+    "revision": "5c755d10078e71356297",
+    "url": "/DFDS/static/js/main.8fcfa97f.chunk.js"
+  },
+  {
+    "revision": "e7ca75f086925e1edc02",
+    "url": "/DFDS/static/js/runtime~main.44fad4e0.js"
+  }
+]);
